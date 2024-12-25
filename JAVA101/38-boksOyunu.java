117c402aa22038fc2a63f6b34d31e3fd0380bda5
Hasan TOPCU <93385577+AdemHasanTopcu@users.noreply.github.com>
2024-12-25 20:36:32 +0300

    38-boksOyunu.java

--- JAVA101/38-boksOyunu.java
+++ JAVA101/38-boksOyunu.java
@@ -1,56 +1,86 @@
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    public Fighter(String name, int damage, int health, int weight, double dodge) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
+    }
+
+    public int hit(Fighter foe) {
+        System.out.println("------------");
+        System.out.println(this.name + " => " + foe.name + " " +  this.damage + " hasar vurdu.");
+
+        if (foe.dodge()) {
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+
+        if (foe.health - this.damage < 0)
+            return 0;
+
+        return foe.health - this.damage;
+    }
+
+    public boolean dodge() {
+        double randomValue = Math.random() * 100; 
+        return randomValue <= this.dodge;
+    }
+}
+
 public class Ring {
     Fighter f1;
     Fighter f2;
     int minWeight;
     int maxWeight;
+    Fighter currentFighter; 
 
     public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
         this.f1 = f1;
         this.f2 = f2;
         this.minWeight = minWeight;
         this.maxWeight = maxWeight;
+        this.currentFighter = decideFirstFighter();
     }
 
     public void run() {
-
         if (checkWeight()) {
-
-            Fighter firstFighter = decideFirstFighter();
-            System.out.println("Dövüşe önce " + firstFighter.name + " başlıyor.");
-
             while (f1.health > 0 && f2.health > 0) {
                 System.out.println("======== YENİ ROUND ===========");
 
-                if (firstFighter == f1) {
+               
+                if (currentFighter == f1) {
                     f2.health = f1.hit(f2);
-                    if (isWin()) {
+                    if (isWin()){
                         break;
                     }
-                    f1.health = f2.hit(f1);
+                    currentFighter = f2; 
                 } else {
                     f1.health = f2.hit(f1);
-                    if (isWin()) {
+                    if (isWin()){
                         break;
                     }
-                    f2.health = f1.hit(f2);
+                    currentFighter = f1; 
                 }
 
-                if (isWin()) {
-                    break;
-                }
                 printScore();
             }
 
         } else {
             System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
         }
-
     }
 
-
+   
     public Fighter decideFirstFighter() {
-        double randomValue = Math.random();
-        return randomValue < 0.5 ? f1 : f2;
+        double randomValue = Math.random(); 
+        return randomValue < 0.5 ? f1 : f2; 
     }
 
     public boolean checkWeight() {
@@ -58,10 +88,10 @@ public class Ring {
     }
 
     public boolean isWin() {
-        if (f1.health <= 0) {
+        if (f1.health == 0) {
             System.out.println("Maçı Kazanan : " + f2.name);
             return true;
-        } else if (f2.health <= 0) {
+        } else if (f2.health == 0){
             System.out.println("Maçı Kazanan : " + f1.name);
             return true;
         }
@@ -73,6 +103,15 @@ public class Ring {
         System.out.println("------------");
         System.out.println(f1.name + " Kalan Can \t:" + f1.health);
         System.out.println(f2.name + " Kalan Can \t:" + f2.health);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
     }
 }
 //Sınıfları ayırarak çalıştırınız.
